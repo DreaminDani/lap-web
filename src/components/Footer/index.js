8d9f985b655f9cb1107a672175def34c002da74d
Daniel Sandoval <daniel@desandoval.net>
2019-06-07 07:59:59 -0400

    css modules ftw + sticky footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './footer.scss';
+import { Box } from '@material-ui/core';
+
+const Footer = class extends React.Component {
+  render() {
+    return (
+      <footer>
+        <Box boxShadow={8}>
+          <iframe title="soundcloud player" width="100%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/444594507&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        </Box>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from '../../src/theme';
-import '../components/all.scss'
 import useSiteMetadata from '../components/SiteMetadata'
 import Footer from '../components/Footer';
 
